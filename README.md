6eb09257d50eea9a0ae30a8375b4201ef8c34894
Vladimir Tikhonov <v.tihonov@itransition.com>
2018-02-23 16:58:56 +0300

    Some template updates for local planner

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Setup
 - add `127.0.0.1 roomplanner.light.wrenkitchens.com` to `/etc/hosts`
+- change `@planner-host` in `config/less/hosts.less` to `roomplanner.light.wrenkitchens.com`
 - run `npm i`
 - run `npm run prod`, root privileges may be required to bind port 80.
--- index.js
+++ index.js
@@ -2,24 +2,38 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const template = require('lodash.template');
+const startCase = require('lodash.startcase');
+const values = require('lodash.values');
+
+const PlannerVersion = {
+    Home: 'home',
+    Showroom: 'showroom',
+    Trade: 'trade',
+    Surveyor: 'surveyor',
+};
+
+const VALID_PLANNER_VERSIONS = values(PlannerVersion);
+const WEBPACK_DEV_SERVER_URL = 'http://127.0.0.1:3001';
 
 const app = express();
 
 const htmlTemplateContent = fs.readFileSync(path.join(__dirname, 'index.html.template'));
 const htmlTemplateGenetator = template(htmlTemplateContent);
 
-const VALID_PLANNER_VERSIONS = ['home', 'showroom', 'trade', 'surveyor'];
-
 function renderHtmlForPlannerVersion(plannerVersion) {
     if (!VALID_PLANNER_VERSIONS.includes(plannerVersion)) {
         return `${plannerVersion} is not a valid planner version.`;
     }
 
-    return htmlTemplateGenetator({ plannerVersion });
+    return htmlTemplateGenetator({
+        plannerVersion,
+        plannerVersionTitle: startCase(plannerVersion),
+        assetsUrl: WEBPACK_DEV_SERVER_URL,
+    });
 }
 
 app.get('/', (request, response) => {
-    response.send(renderHtmlForPlannerVersion('home'));
+    response.send(renderHtmlForPlannerVersion(PlannerVersion.Home));
 });
 
 app.get('/:plannerVersion', (request, response) => {

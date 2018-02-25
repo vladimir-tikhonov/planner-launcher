5eb573becfdf8a25798ef99a0be1a14f27bbc6f3
Vladimir Tikhonov <vladimir@tikhonov.by>
2018-02-25 14:07:13 +0300

    More updates for local planner

--- index.js
+++ index.js
@@ -11,33 +11,66 @@ const PlannerVersion = {
     Trade: 'trade',
     Surveyor: 'surveyor',
 };
+const VALID_PLANNER_VERSIONS = new Set(values(PlannerVersion));
 
-const VALID_PLANNER_VERSIONS = values(PlannerVersion);
+const RoomTypeHandle = {
+    Kitchen: 'kitchen',
+};
+const roomTypeIdMapping = {
+    kitchen: 1,
+};
+const VALID_ROOM_TYPE_HANDLES = new Set(values(RoomTypeHandle));
+
+const LaunchParam = {
+    Action: { name: 'action', defaultValue: 'default' },
+    PlanId: { name: 'planId' },
+    AccountId: { name: 'accountId' },
+    Email: { name: 'email' },
+    Username: { name: 'username', defaultValue: 'testit' },
+};
+
+const LAUNCHER_HOST = 'roomplanner.light.wrenkitchens.com';
+const LAUNCHER_PORT = 80;
 const WEBPACK_DEV_SERVER_URL = 'http://127.0.0.1:3001';
 
 const app = express();
+app.use(express.static('static'));
 
 const htmlTemplateContent = fs.readFileSync(path.join(__dirname, 'index.html.template'));
 const htmlTemplateGenetator = template(htmlTemplateContent);
 
-function renderHtmlForPlannerVersion(plannerVersion) {
-    if (!VALID_PLANNER_VERSIONS.includes(plannerVersion)) {
+function renderHtmlForPlannerVersion(plannerVersion, roomTypeHandle, launchParams) {
+    if (!VALID_PLANNER_VERSIONS.has(plannerVersion)) {
         return `${plannerVersion} is not a valid planner version.`;
+    }
+    if (!VALID_ROOM_TYPE_HANDLES.has(roomTypeHandle)) {
+        return `${roomTypeHandle} is not a valid room type.`;
     }
 
     return htmlTemplateGenetator({
         plannerVersion,
         plannerVersionTitle: startCase(plannerVersion),
+        roomTypeHandle,
+        launchParams: JSON.stringify(launchParams),
         assetsUrl: WEBPACK_DEV_SERVER_URL,
     });
 }
 
-app.get('/', (request, response) => {
-    response.send(renderHtmlForPlannerVersion(PlannerVersion.Home));
+app.get('/:plannerVersion?/:roomType?', (request, response) => {
+    const plannerVersion = request.params.plannerVersion || PlannerVersion.Home;
+    const roomTypeHandle = request.params.roomType || RoomTypeHandle.Kitchen;
+
+    const launchParams = {
+        roomId: roomTypeIdMapping[roomTypeHandle],
+    };
+    values(LaunchParam).forEach((paramConfig) => {
+        const paramValue = request.query[paramConfig.name] || paramConfig.defaultValue;
+        if (paramValue) {
+            launchParams[paramConfig.name] = paramValue;
+        }
+    });
+
+    response.send(renderHtmlForPlannerVersion(plannerVersion, roomTypeHandle, launchParams));
 });
 
-app.get('/:plannerVersion', (request, response) => {
-    response.send(renderHtmlForPlannerVersion(request.params.plannerVersion));
-});
-
-app.listen(80, 'roomplanner.light.wrenkitchens.com');
+app.listen(LAUNCHER_PORT, LAUNCHER_HOST);
